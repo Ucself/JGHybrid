007e19422dee33aac883bd309fc6689e9b5f81e5
李保君 <lbj147123@163.com>
2019-08-15 14:52:54 +0800

    添加torem库

---
+++ react_h5/src/pages/appPages/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import './index.css'
+
+export class App extends Component {
+
+    render() {
+        return (
+            <div
+                class="searchResult"
+            >
+                <div
+                    class="item"
+                >
+                    <div class="imagebg">
+                        <img class="image" src="https://avatar-qiniu.doctorwork.com/FmwjPD5TrxKW0nwAsn8uUKkPY1qi" alt="商品图片" />
+                    </div>
+                    <span class="title">
+                        护理一生
+                    </span>
+                    <span class="subtitle">
+                        实物商品-新增-设置规格
+                    </span>
+                    <div class="priceContainer">
+                        <span class="price">
+                            ￥554.99
+                        </span>
+                        <span class="discount">
+                            0.9折
+                        </span>
+                    </div>
+                    <span class="originalPrice">
+                        ￥5555
+                    </span>
+                </div>
+                <div
+                    class="item"
+                >
+                    <div class="imagebg">
+                        <img class="image" src="https://avatar-qiniu.doctorwork.com/FmwjPD5TrxKW0nwAsn8uUKkPY1qi" alt="商品图片" />
+                    </div>
+                    <span class="title">
+                        2、护理一生
+                    </span>
+                    <span class="subtitle">
+                        2、实物商品-新增-设置规格
+                    </span>
+                    <div class="priceContainer">
+                        <span class="price">
+                            ￥554.99
+                        </span>
+                        <span class="discount">
+                            0.9折
+                        </span>
+                    </div>
+                    <span class="originalPrice">
+                        ￥5555
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
--- react_h5/src/router/index.js
+++ react_h5/src/router/index.js
@@ -1,58 +1,10 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link, IndexRoute } from "react-router-dom";
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>App</h1>
-                <ul>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/inbox">Inbox</Link></li>
-                    <li><Link to="/messages/5">Message</Link></li>
-                </ul>
-                {this.props.children}
-            </div>
-        )
-    }
-}
-
-class About extends Component {
-    render() {
-        return <h3>About</h3>
-    }
-}
-
-class Inbox extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Inbox</h2>
-                {this.props.children || "Welcome to your Inbox"}
-            </div>
-        )
-    }
-}
-
-class Message extends Component {
-
-    render() {
-        return <h3>Message {this.props.id}</h3>
-    }
-}
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { App } from "../pages/appPages/index"
 
 const RoutesInfo = [{
     path: '/App',
     component: App
-}, {
-    path: '/about',
-    component: About
-}, {
-    path: '/inbox',
-    component: Inbox
-}, {
-    path: '/messages/:id',
-    component: Message
 }]
 
 const AppRouter = () => {
@@ -64,7 +16,5 @@ const AppRouter = () => {
         </Router>
     );
 };
-
-
 
 export default AppRouter;
